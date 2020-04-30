71a21e0ce5604d008ee9cfebd8121b42e8e3bc4c
FloweyTheFlower420 <50057682+FloweyTheFlower420@users.noreply.github.com>
2020-04-30 15:24:27 -0500

    improved test program

--- test.cpp
+++ test.cpp
@@ -1,19 +1,18 @@
 #include "camera.h"
 
+using namespace world; 				// using namespaces for convenience
+using namespace types;
+
 int main()
 {
-	using namespace world;
-	using namespace types;
-	sf::Image green;
-	sf::Image cyan;
-	green.loadFromFile("green.png");
+	sf::Image green;			// example tile image
+	sf::Image cyan;				// another tile for testing
+	
+	green.loadFromFile("green.png");	// load from file
 	cyan.loadFromFile("cyan.png");
-	tileset_t t{green, cyan};
-	sf::Texture asdf;
-	asdf.loadFromImage(t[0]);
-	sf::Sprite s;
-	s.setTexture(asdf);
-	level_t l{
+	
+	tileset_t t{green, cyan};		// create tileset
+	level_t l{				// create levels, which tells tilemap which tile to use
 		{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, 
 		{0, 1, 1, 1, 1, 0, 1, 0, 0, 1, 0}, 
 		{0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0}, 
@@ -25,22 +24,22 @@ int main()
 		{0, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0}, 
 		{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, 
 		};
-	world::tilemap tmap (t, l, 32);
-	sf::RenderWindow idk(sf::VideoMode(352, 320), "asdf");
+	world::tilemap tmap (t, l, 32);		// create tilemap; 32 is the size of the tiles
+	sf::RenderWindow window(sf::VideoMode(352, 320), "tilemap example");
 
-	tmap.render();
-	while (idk.isOpen())
+	tmap.render(); 				// render tilemap and store to internal buffer
+	while (window.isOpen())
     	{
         	// check all the window's events that were triggered since the last iteration of the loop
         	sf::Event event;
-        	while (idk.pollEvent(event))
+        	while (window.pollEvent(event))
         	{
         	   	 // "close requested" event: we close the window
         	    	if (event.type == sf::Event::Closed)
-        	        	idk.close();
+        	        	window.close();
         	}
-		idk.clear();
-		tmap.draw(&idk, {0,0});
-		idk.display();
+		window.clear();			// clear screen
+		tmap.draw(&window, {0,0});		// redraw the tilemap (very fast)
+		window.display();		// update screen
     	}  
 }
